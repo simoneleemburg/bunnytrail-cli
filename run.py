21228096e28143cc019ac2889956db9c070378cd
Simone Leemburg <leemburg@zorgdomein.nl>
2026-06-25 08:59:24 +0200

    debugging

--- run.py
+++ run.py
@@ -5,6 +5,11 @@ _here = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, _here)
 sys.path.insert(0, os.path.join(_here, "vendor"))
 
+print("_here:", _here)
+print("vendor path:", os.path.join(_here, "vendor"))
+print("vendor exists:", os.path.isdir(os.path.join(_here, "vendor")))
+print("prompt_toolkit exists:", os.path.isdir(os.path.join(_here, "vendor", "prompt_toolkit")))
+
 from bunnytrail_cli.helpers import find_project_root
 from bunnytrail_cli.repl import run_shell
 
