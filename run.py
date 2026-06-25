dcfcd790661051951ddddca01b8958cb9918326a
Simone Leemburg <leemburg@zorgdomein.nl>
2026-06-25 08:52:27 +0200

    bundled in prompt_tools

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+_here = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, _here)
+sys.path.insert(0, os.path.join(_here, "vendor"))
 
-from bunnytrail_cli.main import cli
+from bunnytrail_cli.helpers import find_project_root
+from bunnytrail_cli.repl import run_shell
 
 if __name__ == "__main__":
-    cli()
+    run_shell(find_project_root())
